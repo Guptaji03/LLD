598d1996eaf6b8b2db2cef29c8b89cb408b5e871
Guptaji03 <saguptaji1998@gmail.com>
2026-01-17 17:38:08 +0530

    First commit

---
+++ Splitwise/src/main/java/com/Practice/LLD/SplitWiseApp.java
@@ -0,0 +1,13 @@
+package com.Practice.LLD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitWiseApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitWiseApp.class, args);
+	}
+
+}
